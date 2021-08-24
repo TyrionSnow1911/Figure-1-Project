3ba295b5baee52b496790a82f3891649494913a2
TyrionSnow1911 <glm3531@gmail.com>
2021-08-24 17:25:00 -0600

    Implement fetch feed data endpoint method.

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,7 @@ from models import get_session, User, Profile, FeedData, create_all
 
 
 app = Flask(__name__)
+feed_data = None
 
 
 @app.route("/user/create", methods=["POST"])
@@ -29,12 +30,22 @@ def create_user_endpoint():
     return jsonify({"return": "success"}), 200
 
 
-@app.route("/feed/", methods=["GET"])
+@app.route("/feed", methods=["GET"])
 def fetch_feed_data():
-    pass
+    global feed_data
+
+    session = get_session()
+
+    rows = session.execute("select * from feed_data;").fetchall()
+
+    data = [list(row) for row in rows]
+
+    print(json.dumps({"return": "success", "data": data}))
+    return json.dumps({"return": "success", "data": data}), 200
 
 
 def seed_database():
+    global feed_data
     # read data from feed.json and convert to data object
     cwd = os.getcwd()
     pathToDataFeed = os.path.join(cwd, "data/feed.json")
@@ -42,25 +53,27 @@ def seed_database():
         data = json.load(f)
 
     # store data in app.db
-    feed = FeedData()
+    feed_data = FeedData()
     count = 0
     for row in data:
         session = get_session()
-        feed.row_id = str(count)
-        feed.caption = row["caption"]
-        feed.followers = row["followers"]
-        feed.image_url = row["image_url"]
-        feed.likes = row["likes"]
-        feed.profile_image_url = row["profile_image_url"]
-        feed.title = row["title"]
-        feed.username = row["username"]
-        u = session.merge(feed)
+        feed_data.row_id = str(count)
+        feed_data.caption = row["caption"]
+        feed_data.followers = row["followers"]
+        feed_data.image_url = row["image_url"]
+        feed_data.likes = row["likes"]
+        feed_data.profile_image_url = row["profile_image_url"]
+        feed_data.title = row["title"]
+        feed_data.username = row["username"]
+        u = session.merge(feed_data)
         session.commit()
         count += 1
+    session.close()
 
 
 if __name__ == "__main__":
     if not os.path.exists("app.db"):
         create_all()
     seed_database()
+    fetch_feed_data()
     # app.run(host="0.0.0.0", port=5000, debug=True)
--- backend/models/models.py
+++ backend/models/models.py
@@ -28,11 +28,3 @@ class FeedData(Base):
     title = Column(Text)
     username = Column(Text)
 
-    # "caption": "Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.\n\nIn sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.",
-    # "followers": 46,
-    # "image_url": "http://dummyimage.com/1243x113.png/ff4444/ffffff",
-    # "likes": 411,
-    # "profile_image_url": "http://dummyimage.com/100x100.png/ff4444/ffffff",
-    # "title": "Quam nec dui",
-    # "username": "jcranstone0"
-
